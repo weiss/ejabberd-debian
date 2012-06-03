a30571d44c6d8a762ffb84962b7fb76184389ea7
Konstantin Khomoutov <flatworm@users.sourceforge.net>
2012-06-03 20:31:31 +0400

    Add simple test suite
    Exports test:test/0 which just runs
    xmerl_regexp:sh_to_awk/1 and ejabberd_regexp:sh_to_awk/1
    on the same pattern and then uses the "result specification"
    for that pattern to decide whether the test passed or not.
    
    There are semantic differences between our and the reference
    implementations though; test suite highlights them but it's
    not clear how to handle them in a sensible manner.

---
+++ test.erl
@@ -0,0 +1,54 @@
+-module(test).
+-compile(export_all).
+-import(lists).
+
+test() ->
+	lists:foreach(fun({Pattern, Res}) ->
+				Orig = xmerl_regexp:sh_to_awk(Pattern),
+				New = ejabberd_regexp:sh_to_awk(Pattern),
+				case Res of
+					true ->
+						case Orig == New of
+							true ->
+								ok;
+							false ->
+								io:format("FAILED: for ~p, ~p =/= ~p~n",
+									[Pattern, Orig, New])
+						end;
+					{That, This} ->
+						case That == Orig of
+							true ->
+								ok;
+							false ->
+								io:format("ORIG FAILED: for ~p, ~p =/= ~p~n",
+									[Pattern, That, Orig])
+						end,
+						case This == New of
+							true ->
+								ok;
+							false ->
+								io:format("NEW FAILED: for ~p, ~p =/= ~p~n",
+									[Pattern, This, New])
+						end
+				end
+		end, [
+			{"", true},
+			{"aaa", true},
+			{"?", true},
+			{"*", true},
+			{"*?", true},
+			{"?*", true},
+			{"a?b*c", true},
+			{"[a-z]", true},
+			{"[0-9]", true},
+			{"[!0-9]", true},
+			{"^hey|\\z(xxx)$", true},
+			{"\\a[\\bc]\\z", true},
+			{"a[0-9]{4}", {"^(a[0-9]{4})$", "^(a[0-9]\\{4\\})$"}},
+			{"a[0-", {"^(a[0-)$", {error, unclosed_be}}},
+			{"[0\\[]", true},
+			{"[0\\]]", true},
+			{"]", true},
+			{"[]", {"^([])$", {error, empty_be}}}
+		]).
+
